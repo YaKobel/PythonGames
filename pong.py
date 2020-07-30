a777070fd17ab2cbeb9e242249774f85ca11420a
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-30 09:05:03 +0300

    create ball and racket yellow

--- pong.py
+++ pong.py
@@ -6,14 +6,31 @@ win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
 
-# racket
+# racket blue
 racket_a = turtle.Turtle()
 racket_a.speed(0)
 racket_a.shape("square")
 racket_a.color("blue")
 racket_a.shapesize(stretch_len=1, stretch_wid=5)
 racket_a.penup()
-racket_a.goto(-350,0)
+racket_a.goto(-350, 0)
+
+# racket yellow
+racket_b = turtle.Turtle()
+racket_b.speed(0)
+racket_b.shape("square")
+racket_b.color("yellow")
+racket_b.shapesize(stretch_len=1, stretch_wid=5)
+racket_b.penup()
+racket_b.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 
 while True:
