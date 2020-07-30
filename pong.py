d41779ddeb3df780ebf668252941834142ccc921
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-30 08:51:13 +0300

    create racket blue

--- pong.py
+++ pong.py
@@ -6,5 +6,15 @@ win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
 
+# racket
+racket_a = turtle.Turtle()
+racket_a.speed(0)
+racket_a.shape("square")
+racket_a.color("blue")
+racket_a.shapesize(stretch_len=1, stretch_wid=5)
+racket_a.penup()
+racket_a.goto(-350,0)
+
+
 while True:
     win.update()
