53017df1a5a2dbbd4c370185ef5485bc295dcb64
Yaroslav Kobel <yarokobel@gmail.com>
2020-08-11 00:27:57 +0300

    update display

--- DinoGame.py
+++ DinoGame.py
@@ -6,6 +6,10 @@ display_width = 800
 display_height = 600
 
 display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Run Dino! Run!')
+
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
 
 
 def run_game():
