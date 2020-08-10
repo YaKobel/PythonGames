0c9da3c8a0abb4259791c6673d479d14cc8930c8
Yaroslav Kobel <yarokobel@gmail.com>
2020-08-10 23:48:58 +0300

    Create Dini Game

---
+++ DinoGame.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+def run_game():
+    game = True
+
+    while game:
+        for event in pygame.even.get():
+            if even.type == pygame.QUIT:
+                pygame.quit()
+                quit(
+
+run_game()
+                )
