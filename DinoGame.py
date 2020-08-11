2b7820203b346a8ce9ab073de865cb6d99e0af84
Yaroslav Kobel <yarokobel@gmail.com>
2020-08-11 07:19:22 +0300

    create cactus

--- DinoGame.py
+++ DinoGame.py
@@ -16,6 +16,12 @@ usr_height = 100
 usr_x = display_width // 3
 usr_y = display_height - usr_height - 100
 
+cactus_width = 20
+cactus_height = 70
+cactus_x = display_width - 50
+cactus_y = display_height - cactus_height - 100
+
+
 clock = pygame.time.Clock()
 
 make_jump = False
@@ -41,6 +47,7 @@ def run_game():
 
 
         display.fill((255, 255, 255))
+        draw_cactus()
 
         pygame.draw.rect(display, (247, 240, 22), (usr_x, usr_y, usr_width, usr_height))
 
@@ -56,6 +63,15 @@ def jump():
     else:
         jump_counter = 30
         make_jump = False
+
+def draw_cactus():
+    global cactus_x, cactus_y, cactus_width, cactus_height
+
+    if cactus_x >= -cactus_width:
+        pygame.draw.rect(display, (224, 44, 64), (cactus_x, cactus_y, cactus_width, cactus_height))
+        cactus_x -= 4
+    else:
+        cactus_x = display_width - 50
 
 
 
