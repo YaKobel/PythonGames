6aba0916b63eca8937818fea34996bf59a6673ee
Yaroslav Kobel <yarokobel@gmail.com>
2020-09-13 15:45:11 +0300

    fix code

--- DinoGame.py
+++ DinoGame.py
@@ -126,7 +126,7 @@ class Bullet:
         self.x += self.speed_x
         self.y += self.speed_y
 
-        if self.x <= self.dest_x and self.y >= self.dest_y:
+        if self.x <= display_width and self.y >= 0:
             display.blit(bullet_img, (self.x, self.y))
             return True
         else:
