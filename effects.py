6d70d917bbfb63348c7233b4bfb1f81c8bb10c89
Yaroslav Kobel <yarokobel@gmail.com>
2020-09-29 23:54:02 +0300

    edit class Bullet

--- effects.py
+++ effects.py
@@ -2,7 +2,7 @@ from parameters import *
 from images import light_img
 
 
-def print_text(message, x, y, font_color=(0, 0, 0),  font_type='PingPong.ttf', font_size=30):
+def print_text(message, x, y, font_color=(0, 0, 0),  font_type='Background/PingPong.ttf', font_size=30):
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
     display.blit(text, (x, y))
