7581e834830f4f5ce64e0578f15e368ad07491bf
Yaroslav Kobel <yarokobel@gmail.com>
2020-09-29 23:40:20 +0300

    edit class objects

--- DinoGame.py
+++ DinoGame.py
@@ -20,30 +20,6 @@ img_counter = 0
 health = 2
 
 
-class Object:
-    def __init__(self, x, y, width, image, speed):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.image = image
-        self.speed = speed
-
-    def move(self):
-        if self.x >= -self.width:
-            display.blit(self.image, (self.x, self.y))
-            self.x -= self.speed
-            return True
-        else:
-            return False
-
-    def return_self(self, radius, y, width, image):
-        self.x = radius
-        self.y = y
-        self.width = width
-        self.image = image
-        display.blit(self.image, (self.x, self.y))
-
-
 class Button:
     def __init__(self, width, height):
         self.width = width
